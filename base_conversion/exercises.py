36a0b827847a2eb0aa20f2c41d03bc0965cb1270
HealingAura32 <vqudus@gmail.com>
2026-02-06 23:17:02 -0500

    exercise 2

--- base_conversion/exercises.py
+++ base_conversion/exercises.py
@@ -6,3 +6,15 @@ def binary_to_decimal(text):
         decimal_form += int(text[i]) * (2**i)
 
     return decimal_form
+
+
+def hexadecimal_to_decimal(text):
+    decimal_form = 0
+    hexadecimal_reference = "0123456789ABCDEF"
+    text = text[::-1]
+    count = 0
+    for i in text:
+        decimal_form += hexadecimal_reference.index(i) * (16**count)
+        count += 1
+
+    return decimal_form
