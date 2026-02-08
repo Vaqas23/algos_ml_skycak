a6dc096b5f481d1931b8366284d92115b2a10534
HealingAura32 <vqudus@gmail.com>
2026-02-08 17:31:37 -0500

    added computation time

--- Chapter 1: Hello World/monte_carlo.py
+++ Chapter 1: Hello World/monte_carlo.py
@@ -1,11 +1,15 @@
 import random
+import time
 
 # Simulates probability of getting heads a certain (num_heads) number of times
 #  when you flip a coin num_flips times. Monte Carlo method.
 
+# added computation time
+
 
 def sim_probability(num_heads, num_flips):
-    num_trials = 10000000
+    start_time = time.time()
+    num_trials = 10000
     successes_overrall = 0
     for i in range(int(num_trials)):
         successes_per_trial = 0
@@ -15,6 +19,9 @@ def sim_probability(num_heads, num_flips):
                 successes_per_trial += 1
         if successes_per_trial == num_heads:
             successes_overrall += 1
+    computation_time = time.time() - start_time
+
+    print(f"Computation time: {round(computation_time, ndigits=3)} seconds")
     return successes_overrall / num_trials
 
 
