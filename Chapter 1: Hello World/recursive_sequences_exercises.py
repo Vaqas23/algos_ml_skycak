bc6606fc453e3d7a46843e4739349d92495f0eb6
HealingAura32 <vqudus@gmail.com>
2026-02-08 15:42:21 -0500

    2.1/2.2

--- Chapter 1: Hello World/recursive_sequences_exercises.py
+++ Chapter 1: Hello World/recursive_sequences_exercises.py
@@ -23,3 +23,43 @@ def one_two(n):
     else:
         previous_term = one_two(n-1)
         return (3 * previous_term) - 4
+
+# Starting with 25, generate each term by taking half of the
+# previous term if it’s even, or multiplying by 3 and adding 1 if
+# it’s odd. (This is an instance of a Collatz sequence.)
+
+# return an array of n terms
+
+
+def two_one(n):
+    terms = [25]
+
+    while len(terms) < n:
+        previous_term = terms[-1]
+
+        if previous_term % 2 == 0:
+            next_term = previous_term // 2
+            terms.append(next_term)
+        else:
+            next_term = (previous_term * 3) + 1
+            terms.append(next_term)
+
+    return terms
+
+# return the nth term
+
+
+def two_two(n):
+    if n == 1:
+        print(n)
+        return 25
+    else:
+        print(n)
+        previous_term = two_two(n-1)
+        if previous_term % 2 == 0:
+            return previous_term // 2
+        else:
+            return (previous_term * 3) + 1
+
+
+print(two_two(5))
