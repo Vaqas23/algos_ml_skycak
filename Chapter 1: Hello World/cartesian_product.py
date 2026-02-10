b0356175b84c0eebb371cb2dfa935ca60154ff4c
HealingAura32 <vqudus@gmail.com>
2026-02-10 16:44:00 -0500

    checkpoint save

--- Chapter 1: Hello World/cartesian_product.py
+++ Chapter 1: Hello World/cartesian_product.py
@@ -9,13 +9,33 @@ def calc_cartesian_product(ranges):
         []
     ]
 
-    # single range could refer to [a] within [[a],[1,2,3]]
+    # single range could refer to [a] within [[a],[1,2]]
     for single_range in ranges:
-
+        print(f"Single range = {single_range}")
         # create a copy of points
         points_copy = []
         for item in points:
             points_copy.append(item)
+        print(f"points copy  = {points_copy}")
+        # points_copy = [ [] ]
+
+        for single_item in single_range:
+            print(f"Single item = {single_item}")
+            # create points extended
+            points_extended = []
+            for item in points_copy:
+                points_extended.append(item)
+
+            print(f"points copy  = {points_copy}")
+            # now make a copy of points, add a single range to it, and add it to points extended.
+            for item_index in range(len(points_copy)):
+                points_extended[item_index].append(single_item)
+            print(f"points extended  = {points_extended}")
+
+            # add points extended to points copied
+            for item_index in range(len(points_extended)):
+                points_copy[item_index] = points_extended[item_index]
+            print(f"points copy  = {points_copy}")
 
         # copy all the copy points into the original points
         for item_index in range(len(points_copy)):
@@ -24,22 +44,4 @@ def calc_cartesian_product(ranges):
     return (points)
 
 
-points = [
-    [1]
-]
-
-print(points)
-
-points_copy = []
-for item in points:
-    points_copy.append(item)
-
-print(points_copy)
-
-for item_index in range(len(points_copy)):
-    points[item_index] = points_copy[item_index]
-
-print(points)
-
-
-# print(calc_cartesian_product([[1, 2, 3], ['a', 'b']]))
+print(calc_cartesian_product([[1, 2, 3], ['a', 'b']]))
