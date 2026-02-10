5f3f32cb67c04e2a154793606b7ee2cc3ee9bf8d
HealingAura32 <vqudus@gmail.com>
2026-02-10 17:18:06 -0500

    completely overthought problem, was trying to copy and mutate in place

--- Chapter 1: Hello World/cartesian_product.py
+++ Chapter 1: Hello World/cartesian_product.py
@@ -2,7 +2,6 @@
 # Loop through each mini array in input
 # make a copy of a given mini array
 
-# input in the form of [ [a], [1,2,3]...]
 def calc_cartesian_product(ranges):
     # what we will return
     points = [
@@ -11,37 +10,16 @@ def calc_cartesian_product(ranges):
 
     # single range could refer to [a] within [[a],[1,2]]
     for single_range in ranges:
-        print(f"Single range = {single_range}")
-        # create a copy of points
-        points_copy = []
-        for item in points:
-            points_copy.append(item)
-        print(f"points copy  = {points_copy}")
-        # points_copy = [ [] ]
+        # build a new list of points for this range
+        new_points = []
+        for point in points:
+            for single_item in single_range:
+                new_point = list(point)
+                new_point.append(single_item)
+                new_points.append(new_point)
+        points = new_points
 
-        for single_item in single_range:
-            print(f"Single item = {single_item}")
-            # create points extended
-            points_extended = []
-            for item in points_copy:
-                points_extended.append(item)
-
-            print(f"points copy  = {points_copy}")
-            # now make a copy of points, add a single range to it, and add it to points extended.
-            for item_index in range(len(points_copy)):
-                points_extended[item_index].append(single_item)
-            print(f"points extended  = {points_extended}")
-
-            # add points extended to points copied
-            for item_index in range(len(points_extended)):
-                points_copy[item_index] = points_extended[item_index]
-            print(f"points copy  = {points_copy}")
-
-        # copy all the copy points into the original points
-        for item_index in range(len(points_copy)):
-            points[item_index] = points_copy[item_index]
-
-    return (points)
+    return points
 
 
 print(calc_cartesian_product([[1, 2, 3], ['a', 'b']]))
