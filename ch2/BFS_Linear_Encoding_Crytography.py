1c2702b31606607673f510ff6db09b153bfb7bab
HealingAura32 <vqudus@gmail.com>
2026-02-11 01:51:36 -0500

    finished exercise 2, fixed 1, learned I shouldnt code this late

--- ch2/BFS_Linear_Encoding_Crytography.py
+++ ch2/BFS_Linear_Encoding_Crytography.py
@@ -22,5 +22,23 @@ def encode_string(text, a, b):
     number_form = convert_to_numbers(text)
     encoded_form = []
     for number in number_form:
-        encoded_form.append((a * number) + 3)
+        encoded_form.append((a * number) + b)
     return encoded_form
+
+
+def decode_numbers(nums, a, b):
+    function_applied = []
+    for num in nums:
+        function_applied.append((num-b)/a)
+
+    for num in function_applied:
+        if num < 0 or num > 26:
+            return False
+        elif num % 1 != 0:
+            return False
+
+    input_to_function = []
+    for num in function_applied:
+        input_to_function.append(int(num))
+
+    return convert_to_letters(input_to_function)
