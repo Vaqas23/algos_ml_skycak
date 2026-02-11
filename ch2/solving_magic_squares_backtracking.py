a0978cf4f2f611fbb11f5fa924321c10973eadd8
HealingAura32 <vqudus@gmail.com>
2026-02-11 13:30:56 -0500

    inital start of brute forcing magic squares

---
+++ ch2/solving_magic_squares_backtracking.py
@@ -0,0 +1,9 @@
+def brute_force_magic_squares():
+    digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    square = [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None]
+    ]
+
+    # following will be the 9 nested for loops
