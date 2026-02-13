7a387b012046f0bba29575ae79c5e3289bcde0f1
HealingAura32 <vqudus@gmail.com>
2026-02-13 16:14:19 -0500

    estimates value fast, however never gives exactly 6 decimal places

--- ch2/cube_root_2_newton-raphson_search.py
+++ ch2/cube_root_2_newton-raphson_search.py
@@ -1,2 +1,11 @@
 def newton_raphson(trials):
-    pass
+    guess = 2
+    for i in range(trials):
+        func_value = (guess)**3 - 2
+        tan_fun_value = 3 * ((guess) ** 2)
+        root = guess - (func_value/tan_fun_value)
+        guess = root
+    return guess
+
+
+print(newton_raphson(2))
