857bc4548bec4bf83cc22be77613be58a4c0a532
HealingAura32 <vqudus@gmail.com>
2026-02-13 16:06:40 -0500

    7 trials get you 6 decimal places of precision

--- ch2/cube_root_2_bisection_search.py
+++ ch2/cube_root_2_bisection_search.py
@@ -19,3 +19,5 @@ print(bisection(52))
 # Given how python does floats (64 bits, 15-17 significant decimals), you answer won't change after ~100, whether its 100 vs 1,000,000.
 # Ex. bisection(1000) == bisection(1000000) evaluates to true.
 # 52 trials seems to be the last trial that matters
+
+# print(bisection(7)) to get 6 decimal places of precision
