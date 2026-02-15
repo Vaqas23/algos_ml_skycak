7a56c333cc5ee8d8c3f887138e0476c7a3f0a844
HealingAura32 <vqudus@gmail.com>
2026-02-15 16:50:02 -0500

    fourth done, added extra comments for clarity

--- ch2/single_variable_gradient_descent.py
+++ ch2/single_variable_gradient_descent.py
@@ -13,7 +13,7 @@ def minimize_first_equation(guess, tolerance):  # for f(x) = x**2 + x + 1
 
     return new_guess
 
-
+# tests
 # print(minimize_first_equation(-2, 1e-8))
 # print(minimize_first_equation(0, 1e-8))
 # print(minimize_first_equation(2, 1e-8))
@@ -31,7 +31,7 @@ def maximize_second_equation(guess, tolerance):
 
     return new_guess
 
-
+# tests
 # print(maximize_second_equation(-5, 1e-10))
 # print(maximize_second_equation(0, 1e-10))
 # print(maximize_second_equation(5, 1e-10))
@@ -51,6 +51,30 @@ def minimize_third_equation(guess, tolerance):
     return new_guess
 
 
-print(minimize_third_equation(0, 1e-8))
-print(minimize_third_equation(0.5, 1e-8))
-print(minimize_third_equation(-0.5, 1e-8))
+# tests
+# print(minimize_third_equation(0, 1e-8))
+# print(minimize_third_equation(0.5, 1e-8))
+# print(minimize_third_equation(-0.5, 1e-8))
+
+
+# for f(x) = 3cos(x) + (x**2 * e**sin(x))
+def minimize_fourth_equation(guess, tolerance):
+    learning_rate = 0.1
+    while True:
+        fx_prime = -3 * math.sin(guess) + 2 * guess * math.e**(math.sin(guess)) + \
+            guess ** 2 * math.cos(guess) * math.e**(math.sin(guess))
+        new_guess = guess - learning_rate * fx_prime
+        if abs(fx_prime) < tolerance:
+            break
+        guess = new_guess
+
+    return new_guess
+
+
+# tests
+# print(minimize_fourth_equation(0.5, 1e-8))
+# print(minimize_fourth_equation(0, 1e-8))
+# print(minimize_fourth_equation(-0.5, 1e-8))
+
+# ^^ Gives 3 different minimums, the third gives the global minimum.
+# Demonstrates importance of multiple guesses!
