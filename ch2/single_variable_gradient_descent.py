9fb2160b05a8cbb92e615dba0edb659f094d5a89
HealingAura32 <vqudus@gmail.com>
2026-02-15 16:33:54 -0500

    maximizing second function

--- ch2/single_variable_gradient_descent.py
+++ ch2/single_variable_gradient_descent.py
@@ -1,4 +1,4 @@
-def minimize_first_equation(guess, tolerance):
+def minimize_first_equation(guess, tolerance):  # for f(x) = x**2 + x + 1
     learning_rate = 0.01
 
     while True:
@@ -11,6 +11,24 @@ def minimize_first_equation(guess, tolerance):
     return new_guess
 
 
-print(minimize_first_equation(-2, 1e-8))
-print(minimize_first_equation(0, 1e-8))
-print(minimize_first_equation(2, 1e-8))
+# print(minimize_first_equation(-2, 1e-8))
+# print(minimize_first_equation(0, 1e-8))
+# print(minimize_first_equation(2, 1e-8))
+
+
+# for f(x) = x**3 - x**4 - x**2, points down
+def maximize_second_equation(guess, tolerance):
+    learning_rate = 0.01
+    while True:
+        fx_prime = (3*guess**2) - (4*guess**3) - (2*guess)
+        new_guess = guess + learning_rate * fx_prime  # + because we are finding maximum
+        if abs(fx_prime) < tolerance:
+            break
+        guess = new_guess
+
+    return new_guess
+
+
+# print(maximize_second_equation(-5, 1e-10))
+# print(maximize_second_equation(0, 1e-10))
+# print(maximize_second_equation(5, 1e-10))
