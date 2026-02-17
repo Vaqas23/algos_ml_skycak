b9bac108986f1570aa6547c4edef90dc82741676
HealingAura32 <vqudus@gmail.com>
2026-02-16 20:13:09 -0500

    added multi file and refactored single var gradient file

--- ch2/single_variable_gradient_descent.py
+++ ch2/single_variable_gradient_descent.py
@@ -3,13 +3,12 @@ import math  # required for third equation (includes sin/cos)
 
 def minimize_first_equation(guess, tolerance):  # for f(x) = x**2 + x + 1
     learning_rate = 0.01
-
-    while True:
-        fx_prime = 2 * guess + 1
+    fx_prime = 2 * guess + 1
+    new_guess = guess
+    while abs(fx_prime) >= tolerance:
         new_guess = guess - learning_rate * fx_prime
-        if abs(fx_prime) < tolerance:
-            break
         guess = new_guess
+        fx_prime = 2 * guess + 1
 
     return new_guess
 
@@ -22,12 +21,12 @@ def minimize_first_equation(guess, tolerance):  # for f(x) = x**2 + x + 1
 # for f(x) = x**3 - x**4 - x**2, points down
 def maximize_second_equation(guess, tolerance):
     learning_rate = 0.01
-    while True:
-        fx_prime = (3*guess**2) - (4*guess**3) - (2*guess)
+    fx_prime = (3*guess**2) - (4*guess**3) - (2*guess)
+    new_guess = guess
+    while abs(fx_prime) >= tolerance:
         new_guess = guess + learning_rate * fx_prime  # + because we are finding maximum
-        if abs(fx_prime) < tolerance:
-            break
         guess = new_guess
+        fx_prime = (3*guess**2) - (4*guess**3) - (2*guess)
 
     return new_guess
 
@@ -40,14 +39,15 @@ def maximize_second_equation(guess, tolerance):
 # could do either min or max, since the equation is f(x) = (sin(x))/(1+x**2)
 def minimize_third_equation(guess, tolerance):
     learning_rate = 0.1
-    while True:
-        fx_prime = ((((math.cos(guess) * (1+guess**2)) -
-                    (math.sin(guess)*(2 * guess))) / ((1+guess**2)**2)))
+    fx_prime = ((((math.cos(guess) * (1+guess**2)) -
+                (math.sin(guess)*(2 * guess))) / ((1+guess**2)**2)))
+    new_guess = guess
+    while abs(fx_prime) >= tolerance:
         # just switch from - to + to find the maximum (same abs value anyway)
         new_guess = guess - learning_rate * fx_prime
-        if abs(fx_prime) < tolerance:
-            break
         guess = new_guess
+        fx_prime = ((((math.cos(guess) * (1+guess**2)) -
+                    (math.sin(guess)*(2 * guess))) / ((1+guess**2)**2)))
     return new_guess
 
 
@@ -60,13 +60,14 @@ def minimize_third_equation(guess, tolerance):
 # for f(x) = 3cos(x) + (x**2 * e**sin(x))
 def minimize_fourth_equation(guess, tolerance):
     learning_rate = 0.1
-    while True:
+    fx_prime = -3 * math.sin(guess) + 2 * guess * math.e**(math.sin(guess)) + \
+        guess ** 2 * math.cos(guess) * math.e**(math.sin(guess))
+    new_guess = guess
+    while abs(fx_prime) >= tolerance:
+        new_guess = guess - learning_rate * fx_prime
+        guess = new_guess
         fx_prime = -3 * math.sin(guess) + 2 * guess * math.e**(math.sin(guess)) + \
             guess ** 2 * math.cos(guess) * math.e**(math.sin(guess))
-        new_guess = guess - learning_rate * fx_prime
-        if abs(fx_prime) < tolerance:
-            break
-        guess = new_guess
 
     return new_guess
 
