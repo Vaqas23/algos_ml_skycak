09484babe9c339f2733475f26bf87f7fbd7f94ed
HealingAura32 <vqudus@gmail.com>
2026-02-19 19:58:20 -0500

    bubble sort

--- ch2/sorting_algos_from_scratch.py
+++ ch2/sorting_algos_from_scratch.py
@@ -14,3 +14,18 @@ def selection_sort(arr):
         sorted_arr.append(min_value)
         arr.remove(min_value)
     return sorted_arr
+
+
+def bubble_sort(arr):
+    while True:  # if swap happens, swap = True, else swap = False, break
+        swap = False
+        for i in range(1, len(arr)):
+            if arr[i] < arr[i-1]:
+                smaller = arr[i]
+                bigger = arr[i-1]
+                arr[i-1] = smaller
+                arr[i] = bigger
+                swap = True
+        if swap == False:
+            break
+    return arr
