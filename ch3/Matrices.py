6654ee0b72b954cdd2424284fb9f05a4af1b3f6a
HealingAura32 <vqudus@gmail.com>
2026-02-21 14:30:29 -0500

    fixed error handling for instantiating matrix, added show method

--- ch3/Matrices.py
+++ ch3/Matrices.py
@@ -1,11 +1,12 @@
 class Matrix:
 
     def __init__(self, arr):
+        self.matrix = arr
         self.num_row = 0
         self.num_col = 0
 
         for i in range(len(arr)):
-            if arr[0] != arr[i]:
+            if len(arr[0]) != len(arr[i]):
                 raise TypeError(
                     "Each row must have an equal number of columns")
 
@@ -15,7 +16,8 @@ class Matrix:
             self.num_col += 1
 
     def show(self):
-        pass
+        for row in self.matrix:
+            print(f"{row}")
 
     def transpose(self):
         pass
@@ -28,3 +30,7 @@ class Matrix:
 
     def scalar_multiply(self):
         pass
+
+
+matrix = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+matrix.show()
